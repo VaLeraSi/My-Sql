d0994e5b56e7826a2c3d49f7cefb2be79e1614c3
VaLeraSi <74680565+VaLeraSi@users.noreply.github.com>
2021-06-17 16:50:15 +0300

    practice_6

---
+++ lesson_6.sql
@@ -0,0 +1,35 @@
+USE vk;
+-- Задание 2
+SELECT
+    from_user_id
+    , COUNT(*) as send 
+FROM messages 
+WHERE to_user_id=328
+GROUP BY from_user_id
+ORDER BY send DESC;
+
+-- Задание 3
+
+SELECT COUNT(*) as 'Likes' FROM profiles WHERE (YEAR(NOW())-YEAR(birthday)) < 10;
+
+-- Задание 4
+
+SELECT gender, COUNT(*) as 'Кол-во' FROM profiles GROUP BY gender;
+    
+-- Задание 5
+
+SELECT count(user_id) FROM media m GROUP BY user_id ;
+
+
+SELECT count(initiator_user_id), initiator_user_id 
+FROM friend_requests fr GROUP BY  initiator_user_id  ORDER BY count(initiator_user_id);
+SELECT count(user_id)as coun, user_id  FROM likes GROUP BY user_id ORDER BY coun; 
+
+SELECT count(from_user_id) as coun, from_user_id FROM messages m GROUP BY from_user_id ORDER BY coun;
+
+
+
+SELECT user_id FROM likes WHERE user_id in (select from_user_id FROM messages where from_user_id 
+in (select initiator_user_id FROM friend_requests fr GROUP BY initiator_user_id  ORDER BY count(initiator_user_id))
+GROUP BY from_user_id ORDER BY count(from_user_id)) GROUP BY user_id ORDER BY count(user_id) limit 10;
+
